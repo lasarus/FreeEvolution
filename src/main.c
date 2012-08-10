62e138495d4bb1cdb367f9bd16dddda8b8d16c3a
lasarus <llasarus@gmail.com>
2012-08-10 20:12:36 +0200

    Took some files from PrettyPatterns and changed them to fit with this project.

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <SDL/SDL.h>
+#include <SDL/SDL_opengl.h>
+
+int screen_width = 640, screen_height = 480, screen_bpp = 32;
+
+/* inits SDL and OpenGL */
+int init()
+{
+}
+
+/* main loop */
+int main(int argc, char ** argv)
+{
+}
