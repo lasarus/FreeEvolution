55e8ba4f4f15725e1b9a7a5bec2004142b4c8e02
lasarus <llasarus@gmail.com>
2012-08-11 21:03:45 +0200

    added beginning to cell stage

--- src/stage.c
+++ src/stage.c
@@ -1,17 +1,28 @@
 #include <SDL/SDL.h>
 #include <SDL/SDL_opengl.h>
 
+#include "error_codes.h"
 #include "stage.h"
+
+error_code_t init_player(player_base_t ** player, double x, double y)
+{
+  *player = malloc(sizeof(player_base_t));
+
+  (*player)->x = x;
+  (*player)->y = y;
+
+  return ERROR_NONE;
+}
 
 void draw_player(player_base_t * player)
 {
-  glLoadIndentity();
+  glLoadIdentity();
 
   glTranslatef(player->x, player->y, 0);
 
   glBegin(GL_QUADS);
 
-  glColor3f(1f, 1f, 1f);
+  glColor3f(1.f, 1.f, 1.f);
 
   glVertex3f(-16, -16, 0);
   glVertex3f(-16, 16, 0);
@@ -23,13 +34,13 @@ void draw_player(player_base_t * player)
 
 void draw_object(object_base_t * object)
 {
-  glLoadIndentity();
+  glLoadIdentity();
 
-  glTranslatef(object-player->x, object->y, 0);
+  glTranslatef(object->x, object->y, 0);
 
   glBegin(GL_QUADS);
 
-  glColor3f(1f, 1f, 1f);
+  glColor3f(1.f, 1.f, 1.f);
 
   glVertex3f(-16, -16, 0);
   glVertex3f(-16, 16, 0);
---
+++ src/stage_cell.h
@@ -0,0 +1,13 @@
+#ifndef STAGE_CELL_H
+#define STAGE_CELL_H
+
+typedef struct stage_cell
+{
+  struct stage_base base;
+
+  player_base_t player;
+} stage_cell_t;
+
+error_code_t stage_cell_init(stage_base_t ** stage);
+
+#endif
