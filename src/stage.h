fdb1e01e9de40b80bc28deba1eca799df11dab28
lasarus <llasarus@gmail.com>
2012-08-11 05:03:50 +0200

    I added some bases for the different stages

---
+++ src/stage.h
@@ -0,0 +1,46 @@
+#ifndef STAGE_H
+#define STAGE_H
+
+typedef enum stage_type
+  {
+    STAGE_NULL = 0,
+    STAGE_CELL
+  } stage_type_t;
+
+typedef enum object_type
+  {
+    OBJECT_NULL = 0,
+    OBJECT_AI
+  } object_type_t;
+
+typedef struct object_base
+{
+  object_type_t type;
+  double x;
+  double y;
+} object_base_t;
+
+typedef struct player_base
+{
+  double x, xv;
+  double y, yv;
+} player_base_t;
+
+typedef struct world_base
+{
+  Uint32 object_count;
+  object_base_t * objects;
+} world_base_t;
+
+typedef struct stage_base
+{
+  stage_type_t type;
+
+  error_code_t (*update)(stage_base_t * self, Uint32 delta);
+  error_code_t (*draw)(stage_base_t * self);
+  error_code_t (*free)(stage_base_t * self);
+} stage_base_t;
+
+
+
+#endif
