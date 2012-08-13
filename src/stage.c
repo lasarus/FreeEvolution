57b4c2a28d8fb744ec9112db4b3b1ed2d29296f1
lasarus <llasarus@gmail.com>
2012-08-14 00:37:01 +0200

    added code for drawing, allocating and freeing skeleton models

--- src/stage.c
+++ src/stage.c
@@ -4,6 +4,41 @@
 #include "error_codes.h"
 #include "stage.h"
 
+error_code_t init_creature_model(creature_model_t ** model)
+{
+  *model = malloc(sizeof(creature_model_t));
+
+  return ERROR_NONE;
+}
+
+void draw_creature_model(creature_model_t * model, double x, double y)
+{
+  int i;
+
+  for(i = 0; i < SKELETON_LEN; i++)
+    {
+      glLoadIdentity();
+
+      glTranslatef(x, y, 0);
+
+      glBegin(GL_QUADS);
+
+      glColor3f(1.f, 1.f, 1.f);
+
+      glVertex3f(-16.0 * model->skeleton[i], -16.0 * model->skeleton[i], 0);
+      glVertex3f(-16.0 * model->skeleton[i], 16.0 * model->skeleton[i], 0);
+      glVertex3f(16.0 * model->skeleton[i], 16.0 * model->skeleton[i], 0);
+      glVertex3f(16.0 * model->skeleton[i], -16.0 * model->skeleton[i], 0);
+
+      glEnd();
+    }
+}
+
+void free_creature_model(creature_model_t * model)
+{
+  free(model);
+}
+
 error_code_t init_player(player_base_t ** player, double x, double y)
 {
   *player = malloc(sizeof(player_base_t));
