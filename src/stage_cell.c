70d5d88e079330cd0a2c5e112e589c11c626fb17
lasarus <llasarus@gmail.com>
2012-08-17 03:05:51 +0200

    Changed smoother animations

--- src/stage_cell.c
+++ src/stage_cell.c
@@ -91,9 +91,9 @@ error_code_t stage_cell_draw(stage_base_t * self, stage_draw_info_t * info)
 
   draw_world(&(cell_stage->world), info);
 
-  if(pow(cell_stage->player.xv, 2) + pow(cell_stage->player.yv, 2) > .25 * .25)
+  if(pow(cell_stage->player.xv, 2) + pow(cell_stage->player.yv, 2) > .4 * .4)
     animation = ANIMATION_SWIM_FAST;
-  else if(pow(cell_stage->player.xv, 2) + pow(cell_stage->player.yv, 2) > .10 * .10)
+  else if(pow(cell_stage->player.xv, 2) + pow(cell_stage->player.yv, 2) > .2 * .2)
     animation = ANIMATION_SWIM_SLOW;
 
   draw_creature_model(info, &(cell_stage->skeleton),
